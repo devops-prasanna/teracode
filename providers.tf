941ac5660a608cf10746ec0a2f1f473349f31508
devops-prasanna <67885849+devops-prasanna@users.noreply.github.com>
2020-08-08 15:02:22 +0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAZHJ2VYEP7VWAOBGR"
-  secret_key = "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"
+  access_key = "xxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxG"
 }
